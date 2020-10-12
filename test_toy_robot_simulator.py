396dd485acbea4ce8a2932572658606702484c07
test <test@test.com>
2020-10-12 14:55:09 -0400

    Add more features to toy robot simulator

--- test_toy_robot_simulator.py
+++ test_toy_robot_simulator.py
@@ -1,6 +1,6 @@
 import unittest
 
-from toy_robot_simulator import Table
+from toy_robot_simulator import Table, Direction, Movement, Robot, Simulator
 
 
 class TestTable(unittest.TestCase):
@@ -25,6 +25,100 @@ class TestTable(unittest.TestCase):
 
         self.assertFalse(self.table.is_valid_coordinate(-1, -1))
         self.assertFalse(self.table.is_valid_coordinate(4, 5))
+
+
+class TestDirection(unittest.TestCase):
+    def test_is_valid_direction(self):
+        self.assertTrue(Direction.is_valid_direction("NORTH"))
+        self.assertTrue(Direction.is_valid_direction("SOUTH"))
+        self.assertTrue(Direction.is_valid_direction("EAST"))
+        self.assertTrue(Direction.is_valid_direction("WEST"))
+
+        # test for lower cases and mixed cases and extra spacing
+        self.assertTrue(Direction.is_valid_direction("WEST "))
+        self.assertTrue(Direction.is_valid_direction(" WEST "))
+        self.assertTrue(Direction.is_valid_direction(" west "))
+
+        # test for invalid cases
+        self.assertFalse(Direction.is_valid_direction(""))
+        self.assertFalse(Direction.is_valid_direction("YES"))
+
+    def test_rotation_left(self):
+        self.assertEqual(Direction.rotate_left("NORTH"), "WEST")
+        self.assertEqual(Direction.rotate_left("north "), "WEST")
+
+    def test_rotation_right(self):
+        self.assertEqual(Direction.rotate_right("north "), "EAST")
+        self.assertEqual(Direction.rotate_right("south"), "WEST")
+
+        self.assertNotEqual(Direction.rotate_right("south"), "WeST")
+
+
+class TestMovement(unittest.TestCase):
+    def test_forward(self):
+        actual = Movement.forward(1, "NORTH", (0, 0))
+        self.assertEqual(actual, (0, 1))
+
+        actual = Movement.forward(1, "SOUTH", (0, 0))
+        self.assertEqual(actual, (0, -1))
+
+        actual = Movement.forward(1, "EAST", (0, 0))
+        self.assertEqual(actual, (1, 0))
+
+        actual = Movement.forward(1, "WEST", (0, 0))
+        self.assertEqual(actual, (-1, 0))
+
+
+class TestRobot(unittest.TestCase):
+    def setUp(self):
+        self.robot = Robot()
+
+    def test_uninitialized_robot(self):
+        # test uninitialized robot
+        self.assertFalse(self.robot.is_initialized)
+        self.assertFalse(self.robot.move())
+        self.assertFalse(self.robot.left())
+        self.assertFalse(self.robot.right())
+
+        self.assertEqual(str(self.robot), "")
+
+    def test_initialize_robot(self):
+        self.robot.place(1, 2, 'NORTH')
+        self.assertEqual(self.robot.x, 1)
+        self.assertEqual(self.robot.y, 2)
+        self.assertEqual(self.robot.direction, 'NORTH')
+        self.assertTrue(self.robot.is_initialized)
+        self.assertEqual(str(self.robot), "1 2 NORTH")
+
+        self.assertTrue(self.robot.move())
+        self.assertTrue(self.robot.left())
+        self.assertTrue(self.robot.right())
+
+    def test_move(self):
+        pass
+
+    def test_rotation(self):
+        self.robot.place(1, 2, 'NORTH')
+        # complete a full left rotation
+        self.assertTrue(self.robot.left())
+        self.assertTrue(self.robot.direction, 'WEST')
+        self.assertTrue(self.robot.left())
+        self.assertTrue(self.robot.direction, 'SOUTH')
+        self.assertTrue(self.robot.left())
+        self.assertTrue(self.robot.direction, 'EAST')
+        self.assertTrue(self.robot.left())
+        self.assertTrue(self.robot.direction, 'NORTH')
+
+        # complete a full right rotation
+        self.assertTrue(self.robot.right())
+        self.assertTrue(self.robot.direction, 'EAST')
+        self.assertTrue(self.robot.right())
+        self.assertTrue(self.robot.direction, 'SOUTH')
+        self.assertTrue(self.robot.right())
+        self.assertTrue(self.robot.direction, 'WEST')
+        self.assertTrue(self.robot.right())
+        self.assertTrue(self.robot.direction, 'NORTH')
+
 
 
 if __name__ == '__main__':
--- toy_robot_simulator.py
+++ toy_robot_simulator.py
@@ -1,4 +1,7 @@
 class Table:
+    """
+    Functions to help objects like robot in placement on a physical game board
+    """
     def __init__(self):
         self.valid_coordinates = []
 
@@ -20,46 +23,145 @@ class Table:
         return False
 
 
+class Direction:
+    """
+    Functions to assist objects like robots in direction and rotation
+    """
+    @classmethod
+    def is_valid_direction(cls, direction):
+        direction = direction.strip().upper()
+        return direction in ["NORTH", "SOUTH", "EAST", "WEST"]
+
+    @classmethod
+    def rotate_left(cls, curr_direction):
+        curr_direction = curr_direction.strip().upper()
+        mapping = {
+                "NORTH": "WEST",
+                "EAST": "NORTH",
+                "SOUTH": "EAST",
+                "WEST": "SOUTH",
+        }
+        if curr_direction in mapping.keys():
+            return mapping[curr_direction]
+        return ""
+
+    @classmethod
+    def rotate_right(cls, curr_direction):
+        curr_direction = curr_direction.strip().upper()
+        mapping = {
+                "NORTH": "EAST",
+                "EAST": "SOUTH",
+                "SOUTH": "WEST",
+                "WEST": "NORTH",
+            }
+        if curr_direction in mapping.keys():
+            return mapping[curr_direction]
+        return ""
+
+
+class Movement:
+    """
+    Functions to assist objects like robots in movement
+    At the moment, only forward is implemented, but other possible movements
+    like teleport, knockbacks, etc can be implemented
+    """
+    @classmethod
+    def forward(cls, steps, curr_direction, curr_coordinates):
+        if Direction.is_valid_direction(curr_direction):
+            if curr_direction == "NORTH":
+                x, y = curr_coordinates
+                return x, y + steps
+            elif curr_direction == "SOUTH":
+                x, y = curr_coordinates
+                return x, y - steps
+            elif curr_direction == "EAST":
+                x, y = curr_coordinates
+                return x + steps, y
+            elif curr_direction == "WEST":
+                x, y = curr_coordinates
+                return x - steps, y
+
+        # if we cannot find any valid scenario
+        # then just return existing coordinates
+        return curr_coordinates
+
+
 class Robot:
+    """
+    Create a moveable game object
+    Note that collision detection is performed in Simulator class, not Robot class
+    """
     def __init__(self):
-        self.x = None
-        self.y = None
+        self.x, self.y = None, None
         self.direction = None
-        self.is_initialized = None
+        self.is_initialized = False
 
     def place(self, x, y, direction):
         self.is_initialized = True
-        self.x = x
-        self.y = y
+        self.x, self.y = x, y
         self.direction = direction
         return True
 
+    def get_next_move(self):
+        if self.is_initialized:
+            next_coordinates = Movement.forward(1, self.direction,
+                                                (self.x, self.y))
+            return next_coordinates
+        return None, None
+
     def move(self):
         if self.is_initialized:
-            pass
-
-        return True
+            self.x, self.y = self.get_next_move()
+            return True
+        return False
 
     def left(self):
-        pass
+        if self.is_initialized:
+            self.direction = Direction.rotate_left(self.direction)
+            return True
+        return False
 
     def right(self):
-        pass
+        if self.is_initialized:
+            self.direction = Direction.rotate_right(self.direction)
+            return True
+        return False
 
-    def __str__(self):
+    def __repr__(self):
         if self.is_initialized:
             return "{} {} {}".format(self.x, self.y, self.direction)
-
         return ""
 
 
 class Simulator:
     def __init__(self):
         self.table = Table()
+        self.robot = Robot()
         # self.table.init_square_table(5)
         self.table.init_rectangle_table(6, 5)
 
-        self.robot = Robot()
+    def place(self, x, y, direction):
+        if Direction.is_valid_direction(direction):
+            if self.table.is_valid_coordinate((x, y)):
+                return self.robot.place(x, y, direction)
+
+        return False
+
+    def move(self):
+        next_coordinates = self.robot.get_next_move()
+        if self.table.is_valid_coordinate(next_coordinates):
+            return self.robot.move()
+
+        return False
+        
+    def left(self):
+        return self.robot.left()
+
+    def right(self):
+        return self.robot.right()
+
+    def report(self):
+        return self.robot
 
 
 def main():

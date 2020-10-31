061f8fc0907edb1683115a52030566d810be0f9f
test <test@test.com>
2020-10-31 19:52:51 -0400

    Improve cl related test cases

--- test_toy_robot_cli.py
+++ test_toy_robot_cli.py
@@ -11,24 +11,27 @@ class TestCLI(unittest.TestCase):
     def setUp(self):
         self.sim = Simulator
 
-    def test_cli(self):
+    def assert_stdin_stdout_equal(self, stdin_filename, stdout_filename):
+        """
+        helper function to mock stdin and stdout with test files
+        and ensure stdin and stdout is equal
+        """
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            main(input_filename=stdin_filename)
+            with open(stdout_filename) as out:
+                expected_out = "".join(out.readlines())
+                self.assertEqual(mock_stdout.getvalue(), expected_out)
 
-        def assert_stdin_stdout_equal(stdin_filename, stdout_filename):
-            """
-            helper function to mock stdin and stdout with test files
-            and ensure stdin and stdout is equal
-            """
-            with patch('sys.stdout', new=StringIO()) as mock_stdout:
-                main(input_filename=stdin_filename)
-                with open(stdout_filename) as out:
-                    expected_out = "".join(out.readlines())
-                    self.assertEqual(mock_stdout.getvalue(), expected_out)
-
-        assert_stdin_stdout_equal(
+    def test_cli_01(self):
+        self.assert_stdin_stdout_equal(
             "test/test_input01.txt", "test/test_output01.txt")
-        assert_stdin_stdout_equal(
+
+    def test_cli_02(self):
+        self.assert_stdin_stdout_equal(
             "test/test_input02.txt", "test/test_output02.txt")
-        assert_stdin_stdout_equal(
+
+    def test_cli_03(self):
+        self.assert_stdin_stdout_equal(
             "test/test_input03.txt", "test/test_output03.txt")
 
 
